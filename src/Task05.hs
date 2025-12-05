44c395413749ac6f7109cba73825c7573ce72cf5
Fiona Runge <github@runjak.codes>
2025-12-05 09:44:57 +0100

    solve 05-1

---
+++ src/Task05.hs
@@ -0,0 +1,54 @@
+module Main where
+
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Distribution.Compat.Prelude (liftM2, mapMaybe, readMaybe)
+
+exampleFile = "./inputs/05/example.txt"
+
+inputFile = "./inputs/05/input.txt"
+
+type Z = Integer
+
+type Range = (Z, Z)
+
+inRange :: Z -> Range -> Bool
+inRange x (a, b) = x >= a && x <= b
+
+parseRange :: String -> Maybe Range
+parseRange input =
+  let (from, to') = span (/= '-') input
+   in liftM2 (,) (readMaybe from) (readMaybe $ drop 1 to')
+
+parseInput :: String -> (Set Range, [Z])
+parseInput input = do
+  let (ranges', rest) = span (/= "") $ lines input
+      ranges = Set.fromList $ mapMaybe parseRange ranges'
+  (ranges, mapMaybe readMaybe rest)
+
+isFresh :: Set Range -> Z -> Bool
+isFresh ranges i =
+  let wanted = fst $ Set.spanAntitone ((<= i) . fst) ranges
+   in any (inRange i) $ Set.elems wanted
+
+solution1 :: String -> String
+solution1 = show . length . uncurry λ . parseInput
+  where
+    λ :: Set Range -> [Z] -> [Z]
+    λ ranges = filter (isFresh ranges)
+
+main :: IO ()
+main = do
+  putStrLn "Task 05"
+
+  example <- readFile exampleFile
+  input <- readFile inputFile
+
+  putStrLn "solution1:"
+  putStrLn $ "example: " <> solution1 example
+  putStrLn $ "input: " <> solution1 input
+
+  putStrLn "solution2:"
+
+-- putStrLn $ "example: " <> solution2 example
+-- putStrLn $ "input: " <> solution2 input
