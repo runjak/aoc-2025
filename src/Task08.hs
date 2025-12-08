6a8f075c34dd7cf95d6380bd1e89ab078d3d9a1b
Fiona Runge <github@runjak.codes>
2025-12-08 11:42:34 +0100

    solve 08-1

---
+++ src/Task08.hs
@@ -0,0 +1,65 @@
+module Main where
+
+import Data.List ( partition, sortOn, sort )
+import Data.Maybe (mapMaybe)
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Data.String (fromString)
+import Distribution.Compat.Prelude (readMaybe)
+
+exampleFile = "./inputs/08/example.txt"
+
+inputFile = "./inputs/08/input.txt"
+
+type Z = Int
+
+type Vec = [Z]
+
+replace :: Char -> Char -> String -> String
+replace x y = map (\c -> if c == x then y else c)
+
+readInput :: String -> [Vec]
+readInput = map (mapMaybe readMaybe . words . replace ',' ' ') . lines
+
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs (x : xs) = map (x,) xs <> pairs xs
+
+squareDistance :: Vec -> Vec -> Z
+squareDistance a b = sum $ zipWith (\x y -> (^ 2) $ x - y) a b
+
+sortedPairs :: [Vec] -> [(Vec, Vec)]
+sortedPairs = sortOn (uncurry squareDistance) . pairs
+
+type Circuit = Set Vec
+
+connect :: [Circuit] -> (Vec, Vec) -> [Circuit]
+connect circuits (v1, v2) =
+  let (affected, rest) = partition (\s -> Set.member v1 s || Set.member v2 s) circuits
+   in Set.unions (Set.fromList [v1, v2] : affected) : rest
+
+solve1 :: Int -> [Vec] -> Int
+solve1 n = product . take 3 . reverse . sort . map Set.size . foldl connect [] . take n . sortedPairs
+
+test = solve1 10 . readInput <$> readFile exampleFile
+
+solution1 :: Int -> String -> String
+solution1 n = show . solve1 n . readInput
+
+solution2 :: String -> String
+solution2 = const "Not implemented"
+
+main :: IO ()
+main = do
+  putStrLn "Task 07"
+
+  example <- readFile exampleFile
+  input <- readFile inputFile
+
+  putStrLn "solution1:"
+  putStrLn $ "example: " <> solution1 10 example
+  putStrLn $ "input: " <> solution1 1000 input
+
+  putStrLn "solution2:"
+  putStrLn $ "example: " <> solution2 example
+  putStrLn $ "input: " <> solution2 input
